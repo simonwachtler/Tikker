e879ef85f7756ed650262061297aa35592b8abd4
simonwachtler <simon.wachtler@gmail.com>
2021-01-31 17:07:39 +0100

    Initial Commit

---
+++ Tikker/TikkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikkerApp.swift
+//  Tikker
+//
+//  Created by Simon Wachtler on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TikkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
