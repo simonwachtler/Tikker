0d9484c345d67772d60d43bf7e934495e4a50c89
simonwachtler <simon.wachtler@gmail.com>
2021-06-23 13:04:46 +0200

    Explore page first element

--- Tikker/Explore.swift
+++ Tikker/Explore.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ExplorePage: View {
     var body: some View {
-        Text ("Hello World!")
+        NavigationView {
+            ScrollView {
+            HStack {
+                Text("Deine Lebenserwartung")
+                            .foregroundColor(.pink)
+                            .fontWeight(.heavy)
+                            .font(.system(size:25))
+                            .padding(.top, 20)
+                    .padding(.leading, 25)
+                    .padding(.bottom, -4)
+                
+                Spacer()
+            }
+            
+                
+                ZStack {
+                CountdownVOne()
+                }
+                .background(Color.white)
+                .cornerRadius(12)
+                .padding(.horizontal, 20)
+                
+            
+            }.navigationBarTitle("Entdecken")
+        }
     }
 }
 
